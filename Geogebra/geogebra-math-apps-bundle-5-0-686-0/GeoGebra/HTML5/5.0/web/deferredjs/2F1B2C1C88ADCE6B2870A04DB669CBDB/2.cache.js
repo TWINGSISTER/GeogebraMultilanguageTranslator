c85eeb92de8c96e5338ffd2811a0cb9112268ca7
Twingsister <twingsister@gmail.com>
2022-02-04 21:54:56 +0100

    More version

---
+++ Geogebra/geogebra-math-apps-bundle-5-0-686-0/GeoGebra/HTML5/5.0/web/deferredjs/2F1B2C1C88ADCE6B2870A04DB669CBDB/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback2('gwc(3391,1,sim);_.ve=function(){};Qul(Oi)(2);\n//# sourceURL=web-2.js\n')
---
+++ Geogebra/geogebra-math-apps-bundle-5-0-686-0/GeoGebra/HTML5/5.0/webSimple/deferredjs/A0E96C9FADA20E7469884FECB50C6128/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('KCb(1969,1,OQh);_.Fd=function(){};Dfh(dh)(2);\n//# sourceURL=webSimple-2.js\n')
