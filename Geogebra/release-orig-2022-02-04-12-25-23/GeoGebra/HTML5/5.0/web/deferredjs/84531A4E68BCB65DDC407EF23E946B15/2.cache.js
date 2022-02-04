a36b71b606b013f873d038bc530b1cf0684e91cb
Twingsister <twingsister@gmail.com>
2022-02-04 18:37:18 +0100

    several options to test

---
+++ Geogebra/release-orig-2022-02-04-12-25-23/GeoGebra/HTML5/5.0/web/deferredjs/84531A4E68BCB65DDC407EF23E946B15/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback2('Y8c(3596,1,{522:1,1:1});_.Tj=function(){};H7k(Zm)(2);\n//# sourceURL=web-2.js\n')
---
+++ Geogebra/release-orig-2022-02-04-12-25-23/GeoGebra/HTML5/5.0/webSimple/deferredjs/825017EE390D4539B68CC3D547D1823B/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('e6b(2206,1,{441:1,1:1});_.Qh=function(){};Pmh(Uk)(2);\n//# sourceURL=webSimple-2.js\n')
